929684a3905d4bd4269411512bf1ddc6ba1279be
realchoky <realchoky@gmail.com>
2025-06-04 13:32:41 +0900

    123

---
+++ js/ex.35.clock.js
@@ -0,0 +1,33 @@
+// 실습 : 디지털 시계
+
+// 작동 버튼 클릭하면 1초씩 디지털 시계가 움직임
+// 중지 버튼 클릭하면 디지털 시계가 중지
+// clock이라는 id를 가진 p요소내에 아래 형식으로 출력
+// 형식 : XXXX년 XX월 XX일 오전/오후 XX시 XX분 XX초
+
+const clock = document.querySelector('#clock');
+
+let timer;
+
+document.querySelector('#start').addEventListener('click', e => {
+    timer = setInterval(e => {
+        const nowDate = new Date();
+        const ampm = nowDate.getHours()>=12 ? "오후" : "오전";
+        let nowStr = `
+            ${nowDate.getFullYear()}년&nbsp;
+            ${nowDate.getMonth()+1}월&nbsp;
+            ${nowDate.getDate()}일&nbsp;
+            ${ampm}&nbsp;
+            ${nowDate.getHours()}시&nbsp;
+            ${nowDate.getMinutes()}분&nbsp;
+            ${nowDate.getSeconds()}초&nbsp;`;
+        clock.innerHTML = nowStr;
+    }, 1000);
+});
+
+document.querySelector('#stop').addEventListener('click', e => {
+    clearInterval(timer);
+});
+
+
+
