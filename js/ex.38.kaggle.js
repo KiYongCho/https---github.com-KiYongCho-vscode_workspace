a9f24a551d266362aaa787f2174d1e54b510ba66
realchoky <realchoky@gmail.com>
2025-10-14 12:01:45 +0900

    123

--- js/ex.38.kaggle.js
+++ js/ex.38.kaggle.js
@@ -9,6 +9,28 @@
 // 3. 고가, 개장가, 저가, 종가 정렬기능 (asc, desc)
 // 4. 거래량 범위검색 기능
 
+const url = 'https://storage.googleapis.com/kagglesdsdata/datasets/2399146/4050987/Samson.json?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20250609%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20250609T074037Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=599d032c678816d238e9c951898a3494297c545145ccc38040a8771bfcd6aaa071b7e50fd5053919675f160f0d776f8a5d9c4fcc3ecbe2f825c5429d723dd1ca6129a9ca5b26640665ec3feb8fea3ecc3d45a92f9bb6c392d07a025bb1bb0b206cc0ea81d281106253fb16ca3560e490315677e85272415c956cee3749e37d5e59917f2d2ef22d30f0a9d6e7cfc4d787a21f4bae2115ae68be34acab80f4b12eb0d7e58eb4fd4e252d7130ec997fd5293a0537a0cccef73fc665a928de4337be89556d0979f5b16ed36003ade42259f313bb5131aede3a2774471b54054eff3033ad0c10a9621380bd2e426132cfd4c424fc1220928af6058fcca7090041e55f';
+
+const stock = {
+    columns: [],
+    datas: [],
+    async fetchData(url) {
+        return await fetch(url);
+    }
+};
+
+stock.fetchData(url)
+.then(response => response.json())
+.then(data => {
+    for (let prop in data) {
+        stock.columns.push(prop);
+        stock.datas.push(data[prop]);
+    }
+    console.log(stock.columns);
+    console.log(stock.datas);
+});
+
+
 
 
 
